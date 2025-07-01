e18a6a7124ca903b3087fa447d6c51667f504ccc
ArhamX24 <arhamabdullah9045@gmail.com>
2025-07-01 12:40:56 +0530

    fixed bugs

--- src/Components/AdminComponents/PastOrdersPage.jsx
+++ src/Components/AdminComponents/PastOrdersPage.jsx
@@ -37,9 +37,7 @@ const PastOrdersPage = () => {
   return (
     <div className="w-11/12 mx-auto bg-gray-100 rounded-lg shadow-md p-5">
       <h2 className="text-xl font-bold text-gray-800 mb-4">Past Orders</h2>
-      {
-        !orders ? <div className='flex w-full h-4/5 items-center justify-center'><span className="loading loading-bars loading-lg"></span></div>
-        :
+      
       <div className="max-h-96 overflow-y-auto">
       <div className="hidden md:grid md:grid-cols-7 gap-4 border-b border-gray-300 pb-2">
         <h5 className="font-bold text-gray-800 text-left">Bill No</h5>
@@ -50,7 +48,10 @@ const PastOrdersPage = () => {
       </div>
 
 <div className="divide-y divide-gray-600">
-        {orders?.map((order, index) => (
+        {
+        !orders ? <div className='flex w-full h-4/5 items-center justify-center'><span className="loading loading-bars loading-lg"></span></div>
+        :
+        orders?.map((order, index) => (
           <div key={index} className="grid grid-cols-1 md:grid-cols-7 gap-4 py-4">
             <h5 className="text-gray-900 font-medium">{order.billNo}</h5>
             <h5 className="text-gray-900 font-medium capitalize">{order.partyName}</h5>
@@ -61,16 +62,15 @@ const PastOrdersPage = () => {
               Confirmed: {new Date(order.updatedAt).toLocaleDateString("en-GB")}
             </h5>
             <button
-                            className="px-4 py-2 flex items-center justify-center h-10 md:h-14 lg:h-10 bg-gray-200 border border-black text-black rounded-md hover:bg-gray-300 transition-all duration-300 cursor-pointer"
-                            onClick={() => handleViewOrder(order._id)}
-                          >
-                            View Order
-                          </button>
+              className="px-4 py-2 flex items-center justify-center h-10 md:h-14 lg:h-10 bg-gray-200 border border-black text-black rounded-md hover:bg-gray-300 transition-all duration-300 cursor-pointer"
+              onClick={() => handleViewOrder(order._id)}>
+              View Order
+            </button>
           </div>
         ))}
       </div>
       </div>
-        }
+        
       </div>
   );
 };
