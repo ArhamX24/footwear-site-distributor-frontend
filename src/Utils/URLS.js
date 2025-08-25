69ba7ba98b74b142cdac743a26d6abbee1b073bb
ArhamX24 <arhamabdullah9045@gmail.com>
2025-08-25 16:56:50 +0530

    url changes

--- src/Utils/URLS.js
+++ src/Utils/URLS.js
@@ -1,3 +1,3 @@
 // export const baseURL = "https://pinkeyfootwear.in" 
-export const baseURL = "https://footwear-site-distributor-backend-hnla.onrender.com" 
+export const baseURL = "https://footwear-site-distributor-backend-1-221.onrender.com" 
 // export const baseURL = "http://localhost:8080" 
